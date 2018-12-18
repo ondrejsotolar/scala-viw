5ebd5aa50535f34ad2640d3cef06a2f8656f86dc
Ondřej Sotolář <ondrej.sotolar@gmail.com>
2018-12-18 14:47:26 +0100

    Add repeat last command impl

--- src/main/scala/viw/Viw.scala
+++ src/main/scala/viw/Viw.scala
@@ -5,30 +5,49 @@ import viw.internals.State
 
 object Viw {
 
-  def processKey(key: String, state: State): Option[State] = key match {
-    case " " => Mode.TurnViwOn.process(state)
-    case "i" => Mode.TurnViwOff.process(state)
-    case "a" => Mode.Append.process(state)
-    case "o" => Mode.OpenNewline.process(state)
-    case "s" => Mode.Substitute.process(state)
-    case "G" => Mode.GoToLastLine.process(state)
-    case "I" => Mode.InsertInLine.process(state)
-    case "A" => Mode.InsertAfterLine.process(state)
-    case "C" => Mode.DeleteAndExit.process(state)
-    case "h" => Cursor.MoveLeft.process(state)
-    case "l" => Cursor.MoveRight.process(state)
-    case "j" => Cursor.MoveDown.process(state)
-    case "k" => Cursor.MoveUp.process(state)
-    case "0" => Cursor.MoveToStartOfLine.process(state)
-    case "$" => Cursor.MoveToEndOfLine.process(state)
-    case "w" => Cursor.MoveToStartOfNextWord.process(state)
-    case "b" => Cursor.MoveToStartOfWord.process(state)
-    case "e" => Cursor.MoveToEndOfWord.process(state)
-    case "%" => Cursor.MatchBrackets.process(state)
-    case "x" => Modify.Delete.process(state)
-    case "X" => Modify.Backspace.process(state)
-    case "D" => Modify.DeleteLine.process(state)
-    case "J" => Modify.JoinLine.process(state)
-    case _ => Some(state)
+  var prevKey: String = "";
+
+  def processKey(key: String, state: State): Option[State] = {
+    maybeRememberKey(key, state)
+
+    key match {
+      case " " => Mode.TurnViwOn.process(state)
+      case "i" => Mode.TurnViwOff.process(state)
+      case "a" => Mode.Append.process(state)
+      case "o" => Mode.OpenNewline.process(state)
+      case "s" => Mode.Substitute.process(state)
+      case "G" => Mode.GoToLastLine.process(state)
+      case "I" => Mode.InsertInLine.process(state)
+      case "A" => Mode.InsertAfterLine.process(state)
+      case "C" => Mode.DeleteAndExit.process(state)
+      case "h" => Cursor.MoveLeft.process(state)
+      case "l" => Cursor.MoveRight.process(state)
+      case "j" => Cursor.MoveDown.process(state)
+      case "k" => Cursor.MoveUp.process(state)
+      case "0" => Cursor.MoveToStartOfLine.process(state)
+      case "$" => Cursor.MoveToEndOfLine.process(state)
+      case "w" => Cursor.MoveToStartOfNextWord.process(state)
+      case "b" => Cursor.MoveToStartOfWord.process(state)
+      case "e" => Cursor.MoveToEndOfWord.process(state)
+      case "%" => Cursor.MatchBrackets.process(state)
+      case "x" => Modify.Delete.process(state)
+      case "X" => Modify.Backspace.process(state)
+      case "D" => Modify.DeleteLine.process(state)
+      case "J" => Modify.JoinLine.process(state)
+      case "." => repeatLastCommand(state)
+      case _ => Some(state)
+    }
+  }
+
+  def maybeRememberKey(key: String, state: State): Unit = {
+    if (state.mode)
+      prevKey = key
+  }
+
+  def repeatLastCommand(state: State): Option[State] = {
+    if (prevKey != "")
+      processKey(prevKey, state)
+    else
+      Some(state)
   }
 }
